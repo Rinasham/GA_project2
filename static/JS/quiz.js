1272d6cccb37277128a8ad2ef669079939527655
Rinasham <rinashamau@gmail.com>
2022-05-02 07:10:54 +1000

    [fix] disable a tags and buttons after pressed

---
+++ static/JS/quiz.js
@@ -0,0 +1,13 @@
+
+function disableCategories(){
+  aTags = document.getElementsByClassName('disable-btn')
+  for (let i = 0; i < aTags.length; i++) {
+    aTags[i].style.pointerEvents = 'none'
+  }
+}
+function disableAnswers(){
+  answers = document.getElementsByClassName('answer-submit')
+  for (let i = 0; i < answers.length; i++) {
+    answers[i].style.pointerEvents = 'none'
+  }
+}
