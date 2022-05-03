fb115771344ea35dcc417e597ae63b8b7832dcc1
Rinasham <rinashamau@gmail.com>
2022-05-04 04:28:31 +1000

    [add] check admin

--- user.py
+++ user.py
@@ -16,3 +16,33 @@ def get_name(id):
   finally:
     if (conn):
       closeDB(conn,cur)
+
+
+
+
+def check_is_admin(userID):
+  conn, cur = connectToDB()
+  try:
+      cur.execute(f"SELECT is_admin FROM users WHERE id='{userID}'")
+      is_admin = cur.fetchone()[0]
+
+      # if the password is correct, set user-id to session
+      if is_admin:
+          print('This user is admin.')
+
+          return True
+
+      else:
+          print('This user is not admin.')
+          return False
+
+  except TypeError as error:
+      print('No such user.')
+
+  except  (Exception, psycopg2.Error) as error:
+      if (conn):
+          print("Failed to search the data.", error)
+
+  finally:
+      if (conn):
+          closeDB(conn,cur)
