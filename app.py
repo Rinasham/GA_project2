0e0194e4572086c6fd92fd68f5ac009c0dcf2448
Rinasham <rinashamau@gmail.com>
2022-05-04 03:52:27 +1000

    [fix] store quiz data from API  in postgresql

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 
+from unicodedata import category
 import psycopg2
 from flask import Flask, request, redirect, render_template, session, flash
 import requests
 import bcrypt
 ## ------------ other files --------------
-from db_settings import DB_URL, connectToDB, closeDB
-from user import get_name
+from db_settings import DB_URL, connectToDB, closeDB, fetchData, fetchAll, insertData, updateData
+from user import get_name, check_is_admin
 from check_answer import check
 ## ------------ options --------------
 import random
 import json
 import os
-import datetime
+
 
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'This is the secret key for session.')
@@ -123,16 +124,16 @@ def logout():
 
 #----------------------------------------------------------------
 
-def init_quiz():
-    global quiz_list
-    global quiz_count
-    global correct_count
-    global QandA_list
+# def init_quiz():
+#     global quiz_list
+#     global quiz_count
+#     global correct_count
+#     global QandA_list
 
-    quiz_list = []
-    QandA_list = []
-    quiz_count = 10
-    correct_count = 0
+#     quiz_list = []
+#     QandA_list = []
+#     quiz_count = 10
+#     correct_count = 0
 
 #--------------------------------
 
@@ -141,8 +142,7 @@ def init_quiz():
 def index():
     global page
     page = 'top'
-    init_quiz()
-    print(f'the length of the quiz_list is {len(quiz_list)} in /home')
+    # init_quiz()
     userId = session.get('user_id')
     if userId != None:
         user = get_name(userId)
@@ -156,7 +156,7 @@ def index():
 def showContact():
     global page
     page = 'contact'
-    init_quiz()
+    # init_quiz()
     return render_template('contact.html', page='contact')
 
 
@@ -164,13 +164,14 @@ def showContact():
 def showAbout():
     global page
     page = 'about'
-    init_quiz()
+    # init_quiz()
     return render_template('about.html', page=page)
 
 
 #----------------------------- API -----------------------------------
 
 current_category = ''
+
 
 @app.route('/quiz')
 def quiz_top():
@@ -182,21 +183,30 @@ def quiz_top():
 
 @app.route('/quiz/<categoryName>')
 def quiz_main(categoryName):
-    global quiz_list
-    global current_category
-    current_category = categoryName
+    userID = session.get('user_id')
 
-    # url = f'http://localhost:3000/quiz/{categoryName}'
-    url = f'https://project2-node-express.herokuapp.com/quiz/{categoryName}'
-    res = requests.get(url).json() # list
+    # insert a game into games table
+    conn, cur = connectToDB()
+    try:
+        cur.execute(f"INSERT INTO games(player_id, correct_count, category, quiz_count) VALUES({userID}, 0, '{categoryName}', 10) RETURNING id;")
+        conn.commit()
+        game_id = cur.fetchone()[0]
+        session['game_id'] = game_id
+        print('Successfully inserted new game data into table.')
+        print(f'DBから取得したgame idは{game_id}')
 
-    list = random.sample(res, 10)
-    for item in list:
-        quiz_list.append(item)
-
-    print(f'the length of the quiz_list is {len(quiz_list)} in /quiz and quiz count is {quiz_count}')
+    except  (Exception, psycopg2.Error) as error:
+        if (conn):
+            print("Failed to insert new game data", error)
+            text = 'Sorry we could not get data, please try again.'
+            return render_template('success-fail/fail.html', text=text)
+    finally:
+        if (conn):
+            closeDB(conn,cur)
 
     return redirect('/progress')
+
+
 
 
 
@@ -204,10 +214,7 @@ def quiz_main(categoryName):
 #----------------------------Quiz control ------------------------------------
 
 ##------------ variable ---------------
-quiz_list = []
-current_quiz_answer = ''
-quiz_count = 10
-correct_count = 0
+
 options_list = ['a', 'b', 'c', 'd']
 
 ##-------------------------------------
@@ -215,90 +222,90 @@ options_list = ['a', 'b', 'c', 'd']
 
 @app.route('/progress')
 def handle_quiz():
-    global quiz_list
-    global quiz_count
-    global current_quiz_answer
     global page
-
     page = 'quiz'
+
+    userID = session.get('user_id')
+    game_id = session.get('game_id')
+    results = fetchData(f'SELECT category, quiz_count FROM games WHERE id={game_id}')
+    categoryName = results[0]
+    quiz_count = results[1]
+    print('quiz_count: ' + str(quiz_count))
     if quiz_count > 0:
-        print(f'the length of the quiz_list is {len(quiz_list)} in /progress[GET] and quiz count is {quiz_count}')
-        next_quiz = quiz_list[quiz_count -1] # obj
-        current_quiz_answer = next_quiz['correct_answer']
-        # next_quiz.pop()
-        print(str(quiz_count) + " quiz count")
+    ##########################################################
+        url = f'http://localhost:3000/quiz/{categoryName}'
+        # url = f'https://project2-node-express.herokuapp.com/quiz/{categoryName}'
+    ##########################################################
+
+        foundQuiz = requests.get(url).json() # obj
+
+        quizID = foundQuiz['_id']
+        question = foundQuiz['question']
+        answers = foundQuiz['answers']
+        correct_answer = foundQuiz['correct_answer']
+        answer_text = answers[correct_answer]
+
         next_quiz = {
-            'id' : next_quiz['_id'],
-            'question' : next_quiz['question'],
-            'answers' : next_quiz['answers'],
+            'id' : quizID,
+            'question' : question,
+            'answers' : answers,
         }
-        quiz_count -= 1
-        is_admin = session.get('is_admin')
-        return render_template('quiz-main.html', quiz = next_quiz, options_list = options_list, page=page, is_admin=is_admin)
+        insert = insertData(
+            f"""
+            INSERT INTO each_game(player_id, game_id, quiz_id, question, correct_answer, answer_text) VALUES
+            ({userID},{game_id},'{quizID}','{question}','{correct_answer}','{answer_text}');
+            """
+            )
+
+        if insert:
+            is_admin = session.get('is_admin')
+            return render_template('quiz-main.html', quiz = next_quiz, options_list = options_list, page=page, is_admin=is_admin)
+        else:
+            text = 'Sorry, we counlt not insert data.'
+            return render_template('success-fail/fail.html', text=text)
     else:
-        quiz_count = 10
         return redirect('/finish')
+
 
 
 
 
 @app.route('/progress', methods=['POST'])
 def check_answer():
-    global correct_count
     chosen_answer = request.form.get('chosen-answer')
+    game_id = session.get('game_id')
+
+    #get data from DB
+    results = fetchData(f'SELECT correct_count, quiz_count FROM games Where id={game_id}')
+    correct_count = results[0]
+    quiz_count = results[1]
+    current_quiz_answer = fetchData(f'SELECT answer_text FROM each_game WHERE id={game_id}')
 
     # check whether the chosen answer was correct
     checked_answer = check(current_quiz_answer, chosen_answer)
     if checked_answer == True:
-        correct_count += 1
-    print('correct count is ' + str(correct_count))
-    print(f'the length of the quiz_list is {len(quiz_list)} in /progress[POST] and quiz count is {quiz_count}')
+        updateData(f'UPDATE games set correct_count = {correct_count +1} WHERE id={game_id}')
+    updateData(f'UPDATE games set quiz_count = {quiz_count -1} WHERE id={game_id}')
+
     return redirect('/progress')
 
 
-QandA_list = []
+
 
 @app.route('/finish')
 def show_answers():
-    global quiz_list
-    global QandA_list
     global page
-    global current_category
-    global correct_count
-
     page = 'quiz'
+    game_id = session.get('game_id')
 
-    # insert the game data into result table
-    userID = session.get('user_id')
-    current_date = datetime.datetime.now()
+    # get all questions and correct answers of the game
+    quiz_list = fetchAll(f'SELECT question, answer_text FROM each_game WHERE game_id={game_id}')
 
-    # if the user has logged in, insert data and redirect to user page
-    if userID != None:
-        conn, cur = connectToDB()
-        try:
-            cur.execute(f"INSERT INTO results(player_id, all_quiz, correct_answers, category, date) VALUES(%s,%s,%s,%s,%s)",
-                        (userID, 10, correct_count, current_category, current_date))
-            conn.commit()
-            print('Successfully inserted new game record into result table.')
-
-        except  (Exception, psycopg2.Error) as error:
-            if (conn):
-                print("Failed to insert new game data into result table.", error)
-
-        finally:
-            if (conn):
-                closeDB(conn,cur)
-
-
-    # render all the questions and correct answers
-    quiz_list.reverse()
-
+    QandA_list = []
     for item in quiz_list:
-        answer_char = item['correct_answer']
-        answer = item['answers'][answer_char]
         obj = {
-            'question' : item['question'],
-            'answer' : answer
+            'question' : item[0],
+            'answer' : item[1]
         }
         QandA_list.append(obj)
 
@@ -326,8 +333,7 @@ def save_request():
     # insert the game data into result table
     conn, cur = connectToDB()
     try:
-        cur.execute(f"INSERT INTO requests(question, answer_a, answer_b, answer_c, answer_d, correct_answer, category) VALUES(%s,%s,%s,%s,%s,%s,%s)",
-                    (question, answer_a, answer_b, answer_c, answer_d, correct_answer, category))
+        cur.execute(f"INSERT INTO requests(question, answer_a, answer_b, answer_c, answer_d, correct_answer, category) VALUES(question, answer_a, answer_b, answer_c, answer_d, correct_answer, category);")
         conn.commit()
         print('Successfully inserted new request into requests table.')
         text = 'We received your request. Thank you for your idea!'
@@ -349,35 +355,15 @@ def save_request():
 
 @app.route('/admin')
 def show_admin():
-    init_quiz()
+    # init_quiz()
     userID = session.get('user_id')
     print(userID)
     # check whether the user is admin
-    conn, cur = connectToDB()
-    try:
-        cur.execute(f"SELECT is_admin FROM users WHERE id='{userID}'")
-        is_admin = cur.fetchone()[0]
-
-        # if the password is correct, set user-id to session
-        if is_admin:
-            print('This user is admin.')
-
-            return render_template('admin/add.html', category_list = category_list)
-
-        else:
-            print('This user is not admin.')
-            return redirect('/')
-
-    except TypeError as error:
-        print('No such user.')
-
-    except  (Exception, psycopg2.Error) as error:
-        if (conn):
-            print("Failed to search the data.", error)
-
-    finally:
-        if (conn):
-            closeDB(conn,cur)
+    is_admin = check_is_admin(userID)
+    if is_admin == True:
+        return render_template('admin/add.html', category_list = category_list)
+    else:
+        return redirect('/')
 
 
 
@@ -392,8 +378,11 @@ def add_quiz():
     category = request.form.get('category')
 
     # send
-    # url = 'http://localhost:3000/add-quiz'
-    url = 'https://project2-node-express.herokuapp.com/add-quiz'
+##########################################################
+    url = 'http://localhost:3000/add-quiz'
+    # url = 'https://project2-node-express.herokuapp.com/add-quiz'
+##########################################################
+
     data = {
         'question': question,
         'answer_a' : answer_a,
@@ -419,28 +408,38 @@ def add_quiz():
 
 @app.route('/update/<id>')
 def show_update(id):
-    # url = f'http://localhost:3000/get/{id}'
-    url = f'https://project2-node-express.herokuapp.com/get/{id}'
-    res = requests.get(url).json() # list
-    print(res)
+    # check whether the user is admin
+    userID = session.get('user_id')
+    is_admin = check_is_admin(userID)
+    ## if admin == True, allow to access to the update page
+    if is_admin == True:
+        ##########################################################
+        url = f'http://localhost:3000/get/{id}'
+        # url = f'https://project2-node-express.herokuapp.com/get/{id}'
+        ##########################################################
+        res = requests.get(url).json() # list
+        print(res)
 
-    if res:
-        data = {
-            'id' : res['_id'],
-            'question': res['question'],
-            'answer_a' : res['answers']['a'],
-            'answer_b' : res['answers']['b'],
-            'answer_c' : res['answers']['c'],
-            'answer_d': res['answers']['d'],
-            'correct_answer' : res['correct_answer'],
-            'category' : res['category']
-        }
-        print(data['id'])
+        if res:
+            data = {
+                'id' : res['_id'],
+                'question': res['question'],
+                'answer_a' : res['answers']['a'],
+                'answer_b' : res['answers']['b'],
+                'answer_c' : res['answers']['c'],
+                'answer_d': res['answers']['d'],
+                'correct_answer' : res['correct_answer'],
+                'category' : res['category']
+            }
+            print(data['id'])
 
-        return render_template('admin/update.html', data=data, options=options_list, category_list=category_list)
+            return render_template('admin/update.html', data=data, options=options_list, category_list=category_list)
+        else:
+            text = 'Unfortunately we could not get data.'
+            return render_template('success-fail/fail.html', text=text)
+    ## if the user is NOT admin, go back to the main page
     else:
-        text = 'Unfortunately we could not get data.'
-        return render_template('success-fail/fail.html', text=text)
+        return redirect('/')
 
 
 
@@ -466,9 +465,11 @@ def update_quiz():
         'correct_answer' : correct_answer,
         'category' : category
     }
-    print(data)
-    # url = f'http://localhost:3000/update'
-    url = f'https://project2-node-express.herokuapp.com/update'
+
+##########################################################
+    url = f'http://localhost:3000/update'
+    # url = f'https://project2-node-express.herokuapp.com/update'
+##########################################################
 
     res = requests.put(url = url, data = data)
 
@@ -485,26 +486,48 @@ def update_quiz():
 
 #-------------- delete --------------------------
 
-@app.route('/delete')
-def show_delete():
-    reqID =request.form.get('id')
-    print(reqID)
-    return render_template('admin/delete.html', id=reqID)
+@app.route('/delete/<id>')
+def show_delete(id):
+    reqID = id
+
+    # check whether the user is admin
+    userID = session.get('user_id')
+    is_admin = check_is_admin(userID)
+    ## if admin == True, allow to access to the delete page
+    if is_admin == True:
+        return render_template('admin/delete.html', id=reqID)
+    else:
+        return redirect('/')
+
+
 
 
 @app.route('/delete', methods=['POST'])
 def delete_quiz():
     reqID = request.form.get('id')
     print(reqID)
+##########################################################
+    url = f'http://localhost:3000/delete/{reqID}'
+    # url = f'https://project2-node-express.herokuapp.com/delete/{reqID}'
+##########################################################
 
-    return 'ok'
+    res = requests.delete(url = url)
+
+    if res.status_code == 200:
+        print('Status code 200')
+        text = 'Successfully deleted quiz'
+        return render_template('success-fail/success.html', text=text)
+    else:
+        text = 'Unfortunately we could not delete data.'
+        return render_template('success-fail/fail.html', text=text)
+
 
 
 #----------------------------- user page -----------------------------------
 
 @app.route('/account')
 def show_account():
-    init_quiz()
+    # init_quiz()
     userID = session.get('user_id')
     print(userID)
     return render_template('account.html')
--- db_settings.py
+++ db_settings.py
@@ -1,8 +1,8 @@
 import psycopg2
 import os
 
-DB_URL= os.environ.get('DATABASE_URL', 'dbname=quiz_api_db')
-# DB_URL = ('dbname=quiz_api_db')
+# DB_URL= os.environ.get('DATABASE_URL', 'dbname=quiz_api_db')
+DB_URL = ('dbname=quiz_api_db')
 
 def connectToDB():
     conn = psycopg2.connect(DB_URL)
@@ -12,3 +12,83 @@ def connectToDB():
 def closeDB(conn, cur):
     cur.close()
     conn.close()
+
+
+
+
+def fetchData(query):
+    conn, cur = connectToDB()
+    try:
+        cur.execute(query)
+        result = cur.fetchone()
+
+        return result
+
+    except  (Exception, psycopg2.Error) as error:
+        if (conn):
+            print("Failed to search the data.", error)
+            return False
+
+    finally:
+        if (conn):
+            closeDB(conn,cur)
+
+
+def fetchAll(query):
+    conn, cur = connectToDB()
+    try:
+        cur.execute(query)
+        results = cur.fetchall()
+
+        return results
+
+    except  (Exception, psycopg2.Error) as error:
+        if (conn):
+            print("Failed to search the data.", error)
+            return False
+
+    finally:
+        if (conn):
+            closeDB(conn,cur)
+
+
+
+
+
+def insertData(query):
+    conn, cur = connectToDB()
+    try:
+        cur.execute(query)
+        conn.commit()
+        print('Successfully inserted data.')
+        return True
+
+    except  (Exception, psycopg2.Error) as error:
+        if (conn):
+            print("Failed to insert data.", error)
+            return False
+
+    finally:
+        if (conn):
+            closeDB(conn,cur)
+
+
+
+
+
+def updateData(query):
+    conn, cur = connectToDB()
+    try:
+        cur.execute(query)
+        conn.commit()
+        print('Successfully updated new game data.')
+        return True
+
+    except  (Exception, psycopg2.Error) as error:
+        if (conn):
+            print("Failed to update new game data", error)
+            return False
+
+    finally:
+        if (conn):
+            closeDB(conn,cur)
